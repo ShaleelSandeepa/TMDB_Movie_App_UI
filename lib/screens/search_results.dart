991051be503e1ebb04db15065e42726eba934bb5
ShaleelSandeepa <shaleelsandeepa7@gmail.com>
2024-01-12 00:01:02 +0530

    started developing search feature

---
+++ lib/screens/search_results.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SearchResults extends StatefulWidget {
+  const SearchResults({super.key});
+
+  @override
+  State<SearchResults> createState() => _SearchResultsState();
+}
+
+class _SearchResultsState extends State<SearchResults> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("data"),
+      ),
+    );
+  }
+}
